8d442b6d6c173916a1d84d7cfc67a68ca8cb458c
Muhammad Yousif <m.alshf3ee@gmail.com>
2020-07-01 06:16:42 +0300

    The HTTP/HTTPS modules

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-import config, {nodeEnv, logStars} from  './config'
 
-logStars('test')
+import http from "http";
 
-console.log(config,nodeEnv);
+const server = http.createServer();
+server.listen(8080);
+
+server.on("request", (req, res) => {
+  res.write("Hello HTTP!\n");
+  setTimeout(() => {
+    res.write("I Can Stream!\n");
+    res.end();
+  }, 3000);
+});
